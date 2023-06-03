6bac2791ec1c92350fd8f5ff96491fe3934c7361
morais1244 <guilhermearau3@gmail.com>
2023-06-02 21:04:26 -0300

    adicionando todos os templates para edição de entradas, tópicos, urls, funções para forms...

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_list_or_404
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
+
 
 
 # Create your views here.
+def topic(request,topic_id):
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic' : topic, 'entries':entries}
+    return render(request, 'learning_logs/topic.html',context)
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    
+    context = {'topics':topics}
+    return render(request, 'learning_logs/topics.html',context)
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'learning_logs/index.html')
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted; process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
+            return redirect('learning_logs:topics')
+
+    # Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id = topic_id)
+    if request.method !=  'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+
+    # Display a blank or invalid form.
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
